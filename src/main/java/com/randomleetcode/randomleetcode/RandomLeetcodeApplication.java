14339c98b0fc86b194fb5c2b1aabcf0065fa33d1
Jordan <jordpears@gmail.com>
2020-08-13 21:29:27 +0100

    add spring initializer files

---
+++ src/main/java/com/randomleetcode/randomleetcode/RandomLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.randomleetcode.randomleetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomLeetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomLeetcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/randomleetcode/randomleetcode/RandomLeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randomleetcode.randomleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomLeetcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
